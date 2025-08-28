4ffa5388963ddce7c01f9e1fc6256add0060b74b
7oka <kk916114@gmail.com>
2025-08-28 07:00:58 +0300

    add product details

--- src/features/product/components/allProducts/allProducts.tsx
+++ src/features/product/components/allProducts/allProducts.tsx
@@ -1,28 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import axios from "axios";
-import ProductCard from "../productCard/productCard"
-import{Product , getProducts} from "../../services/ProductsServices/ProductsServices"
-
+import ProductCard from "../productCard/productCard";
+import { useProducts } from "../..//hooks/useProducts";
 
 const ProductsList: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getProducts();
-        setProducts(data);
-      } catch (err) {
-        setError("Failed to fetch products");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const {products ,loading ,error}= useProducts();
 
   if (loading) return <p>Loading products...</p>;
   if (error) return <p>{error}</p>;
--- src/features/product/hooks/useProducts.ts
+++ src/features/product/hooks/useProducts.ts
@@ -1,21 +1,19 @@
 import { useEffect , useState } from "react";
 import { Product } from "../services/ProductsServices/ProductsServices";
 import { getProducts } from "../services/ProductsServices/ProductsServices";
-const useProducts = ()=>{
+export const useProducts = ()=>{
     const [products, setproducts] = useState<Product[]>([]);
     const [loading, setLoading] = useState(true);
     const [error , setError] = useState<string | null>(null);
     useEffect(()=>{
     const fetchProducts=async()=>{
         try{
-                    const data = await getProducts();
-                    setproducts(data);
-
+            const data = await getProducts();
+            setproducts(data);
         }
         catch(error)
         {
             setError("faild to fetch products");
-
         }finally{
             setLoading(false);
         }
